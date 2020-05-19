325b65e2908659a9ebc34415783442bc1bed35db
Ryan Modrak <rmodrak@lanl.gov>
2020-05-19 13:23:28 -0600

    Added checkerboard example

---
+++ paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '/home/rmodrak/data/Examples3d/checkers/model_init_gll'
+
+MODEL_TRUE = '/home/rmodrak/data/Examples3d/checkers/model_true_gll'
+
+PRECOND = ''
+
+SPECFEM_DATA = '/home/rmodrak/data/Examples3d/checkers/specfem3d-b5559822/DATA'
+
+SPECFEM_BIN = '/home/rmodrak/data/Examples3d/checkers/specfem3d-b5559822/bin'
+
